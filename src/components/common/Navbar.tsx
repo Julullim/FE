1363c75ae6ecb21b413c8f44f21db290ef66efe0
baebyeolha <qufgk0207@naver.com>
2024-08-20 15:44:26 +0900

    feat: create navbar route

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-//import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import logo from "../../assets/JulAllim.png";
 import bell from "../../assets/common/navbar/p/bell.png";
 import profile from "../../assets/common/navbar/p/profile.png";
@@ -10,27 +10,27 @@ interface NavbarProps {
 }
 
 const Navbar: React.FC<NavbarProps> = ({ text }) => {
-    //const navigate = useNavigate(); 
+    const navigate = useNavigate(); 
 
-    // const handleNavClick = (point:string) =>{
-    //     navigate(`/${point}`)
-    // }
+    const handleNavClick = (point:string) =>{
+        navigate(`/${point}`)
+    }
 
     return (
         <div className="w-[100vw] h-[6.4vh] flex items-center bg-white" style={{ position: 'absolute', top: 0 }}>
-            <img className="ml-[4.2vw] h-[3.1vh]" src={logo} alt="Logo" />
+            <img className="ml-[4.2vw] h-[3.1vh]" src={logo} alt="Logo" onClick={() => handleNavClick('main')}/>
 
             <div className="flex ml-[9.9vw] font-pt text-[2.2vh] text-darkgray gap-[3.8vw] items-center">
-                <div className="w-[5.3vw] text-center">공지사항</div>
-                <div className="w-[5.3vw] text-center">캘린더</div>
-                <div className="w-[5.3vw] text-center">동방예약</div>
-                <div className="w-[5.3vw] text-center">굿즈</div>
-                <div className="w-[5.3vw] text-center">전지</div>
+                <div className="w-[5.3vw] text-center" onClick={() => handleNavClick('notice')}>공지사항</div>
+                <div className="w-[5.3vw] text-center" onClick={() => handleNavClick('calender')}>캘린더</div>
+                <div className="w-[5.3vw] text-center" onClick={() => handleNavClick('reservation')}>동방예약</div>
+                <div className="w-[5.3vw] text-center" onClick={() => handleNavClick('goods')}>굿즈</div>
+                <div className="w-[5.3vw] text-center" onClick={() => handleNavClick('setlist')}>전지</div>
             </div>
 
             <div className="ml-auto mr-[6.4vw] flex items-center">
                 <img src={bell} className="h-[2.1vh]" alt="Bell" />
-                <img src={line} className="ml-[0.4vw] h-[2.1vh]" alt="Line" />
+                <img src={line} className="ml-[0.4vw] h-[2.1vh]" alt="Line"/>
                 <img src={profile} className="ml-[0.9vw] h-[2.1vh]" alt="Profile" />
                 <div className="font-pt text-[1.8vh] text-darkgray">로그인</div>
             </div>
--- src/routers/Mainrouter.tsx
+++ src/routers/Mainrouter.tsx
@@ -20,6 +20,67 @@ export const mainRouter = [
         )
        
     },
+
+    /* 컴포넌트 추후 수정하기 */
+
+    {
+    
+        path: "/main",
+        element:(
+           <LoginPage/>
+        )
+       
+    },
+
+    
+    {
+    
+        path: "/notice",
+        element:(
+           <LoginPage/>
+        )
+       
+    },
+
+    
+    {
+    
+        path: "/calender",
+        element:(
+           <LoginPage/>
+        )
+       
+    },
+
+    
+    {
+    
+        path: "/reservation",
+        element:(
+           <LoginPage/>
+        )
+       
+    },
+
+    
+    {
+    
+        path: "/goods",
+        element:(
+           <LoginPage/>
+        )
+       
+    },
+
+    
+    {
+    
+        path: "/setlist",
+        element:(
+           <LoginPage/>
+        )
+       
+    },
     
     
    
