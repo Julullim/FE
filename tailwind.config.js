9881daabbe5b0ec1bf8e5a10981b9ff91462eb23
baebyeolha <qufgk0207@naver.com>
2024-08-20 19:19:27 +0900

    feat: add navbar bold and text shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,8 @@ export default {
       },
 
       textShadow: {
-        'white-blur-15': '0px 0px 15px rgba(255, 255, 255, 1)', 
+        'white-blur-15': '0px 0px 15px rgba(255, 255, 255, 1)',
+        'black-blur-15' : '0px 0px 5px rgba(0, 0, 0, 0.25)'
       },
 
       colors:{
